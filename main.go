ed006e7cba58bd7a4c0ce6697f353d22c7ac1153
Artyom Belousov <flygrounder@yandex.ru>
2025-06-28 16:06:14 +0300

    Add borders

--- main.go
+++ main.go
@@ -17,23 +17,44 @@ const gap = "\n\n"
 type mode int
 
 const (
-	modeOverview = iota
+	modeOverview mode = iota
 	modeUrl
 	modeResponse
 )
 
+type httpMethod int
+
+func (h httpMethod) toString() string {
+	switch h {
+	case httpMethodGet:
+		return "GET"
+	case httpMethodPost:
+		return "POST"
+	}
+	panic("unknown method")
+}
+
+const (
+	httpMethodGet httpMethod = iota
+	httpMethodPost
+)
+
 type model struct {
 	client *http.Client
+	width  int
+	height int
 
-	currentMode  mode
-	methodIndex  int
-	url          textinput.Model
-	responseView viewport.Model
+	currentMode   mode
+	currentMethod httpMethod
+	url           textinput.Model
+	responseView  viewport.Model
 }
 
 func initialModel() model {
+	url := textinput.New()
+	url.Prompt = ""
 	return model{
-		url:    textinput.New(),
+		url:    url,
 		client: http.DefaultClient,
 	}
 }
@@ -55,7 +76,9 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			case "r":
 				m.currentMode = modeResponse
 			case "m":
-				m.methodIndex = (m.methodIndex + 1) % 2
+				m.currentMethod = (m.currentMethod + 1) % 2
+			case "q":
+				return m, tea.Quit
 			}
 		}
 	case modeUrl:
@@ -66,21 +89,21 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		m.responseView = viewport.New(msg.Width, msg.Height-lipgloss.Height(m.url.View())-lipgloss.Height(gap))
+		m.width = msg.Width
+		m.height = msg.Height
+		m.responseView = viewport.New(msg.Width-2, msg.Height-3-lipgloss.Height(m.renderUrlView()))
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c":
-			return m, tea.Quit
 		case "esc":
 			m.url.Blur()
 			m.currentMode = modeOverview
 		case "enter":
 			var resp *http.Response
 			var err error
-			switch m.methodIndex {
-			case 0:
+			switch m.currentMethod {
+			case httpMethodGet:
 				resp, err = m.client.Get(m.url.Value())
-			case 1:
+			case httpMethodPost:
 				resp, err = m.client.Post(m.url.Value(), "application/json", bytes.NewReader([]byte("{}")))
 			}
 			if err != nil {
@@ -94,19 +117,19 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.responseView.SetContent(string(res))
 		}
 	}
-	var method string
-	switch m.methodIndex {
-	case 0:
-		method = "GET"
-	case 1:
-		method = "POST"
-	}
-	m.url.Prompt = fmt.Sprintf("%s > ", method)
 	return m, tea.Batch(reqCmd, respCmd)
 }
 
 func (m model) View() string {
-	return fmt.Sprintf("%s%s%s", m.url.View(), gap, m.responseView.View())
+	return fmt.Sprintf("%s%s%s", m.renderUrlView(), gap, m.renderResponseView())
+}
+
+func (m model) renderUrlView() string {
+	return lipgloss.NewStyle().Width(m.width - 2).Border(lipgloss.NormalBorder()).Render(m.url.View())
+}
+
+func (m model) renderResponseView() string {
+	return lipgloss.NewStyle().Border(lipgloss.NormalBorder()).Render(m.responseView.View())
 }
 
 func main() {
