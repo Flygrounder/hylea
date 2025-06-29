b537a6fb7727d8e13f170b69a4088a09540a6a0b
Artyom Belousov <flygrounder@yandex.ru>
2025-06-29 19:28:42 +0300

    Add timer

--- main.go
+++ main.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"net/http"
 
+	"github.com/charmbracelet/bubbles/stopwatch"
 	"github.com/charmbracelet/bubbles/textinput"
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
-
-const gap = "\n"
 
 type mode int
 
@@ -40,22 +38,29 @@ const (
 )
 
 type model struct {
-	client *http.Client
-	width  int
-	height int
-
+	client        *http.Client
+	width         int
+	height        int
 	currentMode   mode
 	currentMethod httpMethod
 	url           textinput.Model
 	responseView  viewport.Model
+	elapsed       stopwatch.Model
+	requestTag    int
+}
+
+type responseMessage struct {
+	tag      int
+	response string
 }
 
 func initialModel() model {
 	url := textinput.New()
 	url.Prompt = ""
 	return model{
-		url:    url,
-		client: http.DefaultClient,
+		url:     url,
+		client:  http.DefaultClient,
+		elapsed: stopwatch.New(),
 	}
 }
 
@@ -64,7 +69,8 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var reqCmd, respCmd tea.Cmd
+	var reqCmd, respCmd, httpCmd, timeCmd, elapsedCmd tea.Cmd
+	m.elapsed, elapsedCmd = m.elapsed.Update(msg)
 	switch m.currentMode {
 	case modeOverview:
 		switch msg := msg.(type) {
@@ -88,40 +94,53 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	}
 
 	switch msg := msg.(type) {
+	case responseMessage:
+		if m.requestTag != msg.tag {
+			break
+		}
+		m.responseView.SetContent(msg.response)
+		timeCmd = m.elapsed.Stop()
 	case tea.WindowSizeMsg:
 		m.width = msg.Width
 		m.height = msg.Height
-		m.responseView = viewport.New(msg.Width-2, msg.Height-2-lipgloss.Height(m.renderUrlView()))
+		m.responseView = viewport.New(msg.Width-2, msg.Height-2-lipgloss.Height(m.renderUrlView())-lipgloss.Height(m.renderStatusBar()))
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "esc":
 			m.url.Blur()
 			m.currentMode = modeOverview
 		case "enter":
-			var resp *http.Response
-			var err error
-			switch m.currentMethod {
-			case httpMethodGet:
-				resp, err = m.client.Get(m.url.Value())
-			case httpMethodPost:
-				resp, err = m.client.Post(m.url.Value(), "application/json", bytes.NewReader([]byte("{}")))
+			m.requestTag++
+			httpCmd = func() tea.Msg {
+				var resp *http.Response
+				var err error
+				switch m.currentMethod {
+				case httpMethodGet:
+					resp, err = m.client.Get(m.url.Value())
+				case httpMethodPost:
+					resp, err = m.client.Post(m.url.Value(), "application/json", bytes.NewReader([]byte("{}")))
+				}
+				if err != nil {
+					return nil
+				}
+				defer resp.Body.Close()
+				res, err := io.ReadAll(resp.Body)
+				if err != nil {
+					return nil
+				}
+				return responseMessage{
+					tag:      m.requestTag,
+					response: string(res),
+				}
 			}
-			if err != nil {
-				break
-			}
-			defer resp.Body.Close()
-			res, err := io.ReadAll(resp.Body)
-			if err != nil {
-				break
-			}
-			m.responseView.SetContent(string(res))
+			timeCmd = m.elapsed.Start()
 		}
 	}
-	return m, tea.Batch(reqCmd, respCmd)
+	return m, tea.Batch(reqCmd, respCmd, httpCmd, elapsedCmd, timeCmd)
 }
 
 func (m model) View() string {
-	return fmt.Sprintf("%s%s%s", lipgloss.JoinHorizontal(lipgloss.Top, m.renderMethod(), m.renderUrlView()), gap, m.renderResponseView())
+	return lipgloss.JoinVertical(lipgloss.Left, lipgloss.JoinHorizontal(lipgloss.Top, m.renderMethod(), m.renderUrlView()), m.renderResponseView(), m.renderStatusBar())
 }
 
 func (m model) renderUrlView() string {
@@ -143,6 +162,11 @@ func (m model) renderResponseView() string {
 		style = style.BorderForeground(lipgloss.Color("#ff0000"))
 	}
 	return style.Render(m.responseView.View())
+}
+
+func (m model) renderStatusBar() string {
+	style := lipgloss.NewStyle().Width(m.width - 2).Border(lipgloss.NormalBorder())
+	return style.Render(m.elapsed.View())
 }
 
 func main() {
