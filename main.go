ea27404d4897e8fbf5710e1817534bc1b963be58
Artyom Belousov <flygrounder@yandex.ru>
2025-07-12 12:10:43 +0300

    Extract item

--- main.go
+++ main.go
@@ -17,41 +17,6 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-type item string
-
-func (i item) FilterValue() string {
-	return string(i)
-}
-
-var _ list.Item = item("")
-
-type itemDelegate struct{}
-
-func (i itemDelegate) Height() int {
-	return 1
-}
-
-func (i itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
-	it := listItem.(item)
-	var res string
-	if m.Index() == index {
-		res = ">" + string(it)
-	} else {
-		res = string(it)
-	}
-	_, _ = w.Write([]byte(res))
-}
-
-func (i itemDelegate) Spacing() int {
-	return 0
-}
-
-func (i itemDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd {
-	return nil
-}
-
-var _ list.ItemDelegate = itemDelegate{}
-
 type mode int
 
 const (
